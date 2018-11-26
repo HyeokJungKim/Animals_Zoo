cdc93db293cbc07b3c9aa4ccaf8dee40a1d884ce
github username <github email address>
2018-11-26 15:38:03 -0500

    created a zoo

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,48 @@
 class Zoo
+  attr_reader :name, :location
+
+  @@all = []
+
+  def initialize(zoo_name, location)
+    @zoo_name = zoo_name
+    @location = location
+
+    @@all << self
+  end
+
+  def self.find_by_location(location)
+    Zoo.all.select do |zoo|
+      zoo.location == location
+      #binding.pry
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def animals
+    Animal.all.select do |animal|
+      animal.zoo == self
+    end
+  end
+
+  def animal_species
+    (self.animals.collect do |animal|
+      animal.species
+    end).uniq
+  end
+
+  def find_by_species(species)
+    self.animals.select do |animal|
+      animal.species == species
+    end
+  end
+
+  def animal_nicknames
+    self.animals.collect do |animal|
+      animal.nickname
+    end
+  end
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,12 @@ require 'pry'
 
 #Test your code here
 
+bronx_zoo = Zoo.new("Bronx Zoo","Bronx, NY")
+silver_rat = Animal.new("grey rat", "Rat", 2, bronx_zoo)
+baboon = Animal.new("chimpie", "monkey", 7, bronx_zoo)
+snake = Animal.new("Sam-Doesnt-Like-Snakes", "snake", 6, bronx_zoo)
+penguin = Animal.new("Spottie", "bird", 3, bronx_zoo)
+gold_rat = Animal.new("golden", "Rat", 2, bronx_zoo)
 
 binding.pry
 puts "done"
